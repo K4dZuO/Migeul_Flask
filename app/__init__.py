f8338a4ee8598b7e8137802460bf29fda4eecae0
Ershov Maxim <ershov_mm@taximaxim.ru>
2025-07-14 13:03:09 +0500

    move logging in helpers funcs

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import logging
-import os
 
 from config import Config
+from app import helpers 
 
 
 db = SQLAlchemy()
@@ -26,34 +24,9 @@ def create_app():
     from app.errors import register_erros
     register_erros(app) 
     app.register_blueprint(bp)
-    if not app.debug:
-        if app.config['MAIL_SERVER']:
-            auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-            secure = None
-            if app.config['MAIL_USE_TLS']:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], 
-                subject='Microblog Failure',
-                credentials=auth, 
-                secure=secure)
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
-            
-        if not os.path.exists(Config.LOG_FOLDER):
-            os.mkdir(Config.LOG_FOLDER)
-        file_handler = RotatingFileHandler(f'{Config.LOG_FOLDER}/microblog.log', maxBytes=10240,
-                                        backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
+    
+    helpers.set_email_error_logging(app)
+    helpers.set_file_logging(app)
+        
         
     return app
