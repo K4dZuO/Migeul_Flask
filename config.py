75ef60c071361a1ae83efb24e83b6fa1678fcd28
Ershov Maxim <ershov_mm@taximaxim.ru>
2025-07-24 14:44:00 +0500

    comments functionality

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir=os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ('sqlite:///' + os.path.join(basedir, 'app.db'))
+    SECRET_KEY=os.environ.get('SECRET_KEY') or 'mysecret'
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or ('sqlite:///' + os.path.join(basedir, 'app.db'))
     SOURCE_FOLDER=os.environ['SOURCE_FOLDER']
     USERS_AVATARS=os.environ['USERS_AVATARS']
     LOG_FOLDER=os.environ['LOG_FOLDER']
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['erschofogt@gmail.com']
+    MAIL_SERVER=os.environ.get('MAIL_SERVER')
+    MAIL_PORT=int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    ADMINS=['erschofogt@gmail.com']
 
     POSTS_PER_PAGE=int(os.environ['POSTS_PER_PAGE'])
+    COMMENTS_PER_PAGE=int(os.environ['COMMENTS_PER_PAGE'])
